7acfa417a3367db126d46c88883f13e55b59a50c
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-01-09 20:58:10 -0300

    Update SIOCookie.c

--- src/SIOCookie.c
+++ src/SIOCookie.c
@@ -23,34 +23,23 @@ ssize_t cookie_sio_read(void *c, char *buf, size_t size);
 //int cookie_sio_close(void *c);
 cookie_io_functions_t COOKIE_FNCTS;
 
-struct memfile_cookie  {
-    char   *buf;        /* Dynamically sized buffer for data */
-    size_t  allocated;  /* Size of buf */
-    size_t  endpos;     /* Number of characters in buf */
-    off_t   offset;     /* Current file offset in buf */
-};
-
 int ee_sio_start(u32 baudrate, u8 lcr_ueps, u8 lcr_upen, u8 lcr_usbl, u8 lcr_umode, int vbuftype)
 {
     DPRINTF("%s: start", __func__);
     DPRINTF("%s: sio_init(%u, %u, %u, %u, %u);\n", __func__, baudrate, lcr_ueps, lcr_upen, lcr_usbl, lcr_umode);
 	sio_init(baudrate, lcr_ueps, lcr_upen, lcr_usbl, lcr_umode);
-    struct memfile_cookie EE_SIO_COOKIE;
-    EE_SIO_COOKIE.buf = NULL; // buffer will be used for EE_SIO RX pin in the future
-    EE_SIO_COOKIE.allocated = 0;
-    EE_SIO_COOKIE.offset = 0;
-    EE_SIO_COOKIE.endpos = 0;
     
     COOKIE_FNCTS.read = cookie_sio_read;
     COOKIE_FNCTS.close = NULL;
     COOKIE_FNCTS.seek = NULL;
     COOKIE_FNCTS.write = cookie_sio_write;
-    EE_SIO = fopencookie(&EE_SIO_COOKIE, "w+", COOKIE_FNCTS);
+    EE_SIO = fopencookie(NULL, "w+", COOKIE_FNCTS);
     if (EE_SIO == NULL) {
         DPRINTF("EE_SIO stream is NULL\n");
         return EESIO_COOKIE_OPEN_IS_NULL;
     }
     setvbuf(EE_SIO, EE_SIO_COOKIE.buf, _IONBF, EE_SIO_COOKIE.allocated); // no buffering for this bad boy
+    fprintf(EE_SIO, "%s: finished", __func__);
     return EESIO_SUCESS;
 }
 
