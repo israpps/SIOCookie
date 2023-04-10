d15284f97408733a99ca7fcf760f9a472ec23ca4
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-04-09 23:20:24 -0300

    Update SIOCookie.c

--- src/SIOCookie.c
+++ src/SIOCookie.c
@@ -26,14 +26,19 @@ int ee_sio_start(u32 baudrate, u8 lcr_ueps, u8 lcr_upen, u8 lcr_usbl, u8 lcr_umo
     EE_SIO = fopencookie(NULL, "w+", COOKIE_FNCTS);
     if (hook_stdout)
     {
+        fprintf(EE_SIO, "%s: hooking std streams...\n", __func__);
+        fprintf(EE_SIO, "\tstdout...\n");
         stdout = fopencookie(NULL, "w+", COOKIE_FNCTS);
-        setvbuf(stdout, NULL, _IONBF, 0); // no buffering
+        setvbuf(stdout, NULL, _IOLBF, 0); // no buffering
+        fprintf(EE_SIO, "\tstderr...\n");
+        stderr = fopencookie(NULL, "w+", COOKIE_FNCTS);
+        setvbuf(stderr, NULL, _IOLBF, 0); // no buffering
     }
     if (EE_SIO == NULL) {
         printf("EE_SIO stream is NULL\n");
         return EESIO_COOKIE_OPEN_IS_NULL;
     }
-    setvbuf(EE_SIO, NULL, _IONBF, 0); // no buffering for this bad boy
+    setvbuf(EE_SIO, NULL, _IOLBF, 0); // no buffering for this bad boy
     fprintf(EE_SIO, "%s: finished\n", __func__);
     return EESIO_SUCESS;
 }
